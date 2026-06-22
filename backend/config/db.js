5aabece5352cadbc6ce8730d621082dbf9c3e4fa
DityaManral11 <dityaguesswhy1102@gmail.com>
2026-06-22 13:34:50 +0530

    Connect backend to Aiven MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,17 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "disaster_management",
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 db.connect((err) => {
   if (err) {
     console.error("❌ Database connection failed:", err);
   } else {
-    console.log("✅ MySQL Connected");
+    console.log("✅ Aiven MySQL Connected");
   }
 });
 
